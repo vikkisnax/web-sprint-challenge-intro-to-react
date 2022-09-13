6c799e424b1f9703be1d0c646a915f0a7ea7f56c
Victoria Blancas <vikki6688@gmail.com>
2022-09-13 19:14:36 -0400

    did some outlining. work on Character comp and then style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import axios from 'axios';
-import { response } from 'msw';
-import React, { useState, useEffect } from 'react';
+// remember, App component is usually for the outline of the page. but i'll combine App and another comp that I could make, here.
 
-//me: styles here
+import axios from 'axios';
+import { response } from 'msw'; //they did this for me i believe once I wrote get request. did i use response??
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character';
+
+//would usually go in another component that dealt with each BOX. might need to use column here...
+import { Container, Row } from "reactstrap"; 
+
+
+//me: STYLES here -- basically for the BOXES
 
 
 const App = () => {
@@ -11,10 +18,10 @@ const App = () => {
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // sync up with, if any. <-- *** THINK ABOUT HOW THIS WILL APPLY HERE ***
 
 
-  const [characters, setCharacters] = useState("")
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     axios
@@ -23,17 +30,25 @@ const App = () => {
         setCharacters(response.data);
         // console.log(response);
         console.log(response.data);
-
       })
       .catch((error)=> {
         console.log("The data was not returned", error);
       });
   }, []);
+  //consider if anything needs to go in the dependency array - pg40 in notes
 
 
+  //basically the outline of the site
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+      {/* just put MAP through each character's box here. pass in characters state to give data to Characters component. would usally map through each BOX in another comp, but ig i'll combine it here in the App comp*/}
+      <div> 
+       {characters.map((character) => {
+          return <Character characters={characters} key={character.url} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,14 @@
 // Write your Character component here
 
-//me: so this will be the layout of the info that's in the App.js component ... probably .map in app.js... display name with props. style in app.js
+//me: so this will be the layout of the INFO that's INSIDE the each box (which could be another component, but I put it inside the App component -- which could've just been for an outline of the page -- .map in App.js then). display names and other character info with props. style in App, above the comp function
 
 
 import React from "react";
+//import from reactstrap for styling... 
 
-export default function Character(){
+export default function Character({characters}){
     return "hello"
-}
+
+};
 
 
