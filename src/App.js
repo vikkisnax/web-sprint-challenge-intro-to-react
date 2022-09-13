3d074aa97cbde75cc60bc71d8eb5d9b21c437233
Victoria Blancas <vikki6688@gmail.com>
2022-09-13 17:57:40 -0400

    msw research

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import axios from 'axios';
+import { response } from 'msw';
+import React, { useState, useEffect } from 'react';
+
+//me: styles here
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,6 +12,24 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+
+  const [characters, setCharacters] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        setCharacters(response.data);
+        // console.log(response);
+        console.log(response.data);
+
+      })
+      .catch((error)=> {
+        console.log("The data was not returned", error);
+      });
+  }, []);
+
 
   return (
     <div className="App">
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+
+//me: so this will be the layout of the info that's in the App.js component ... probably .map in app.js... display name with props. style in app.js
+
+
+import React from "react";
+
+export default function Character(){
+    return "hello"
+}
+
+
